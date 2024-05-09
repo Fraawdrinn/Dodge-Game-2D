c4143eb3737c18c4e5fd3ad6226ae3d7812b3f4c
Fraawdrinn <107624873+Fraawdrinn@users.noreply.github.com>
2024-05-09 12:18:12 +0000

    added world class

--- Game/main.py
+++ Game/main.py
@@ -7,38 +7,58 @@ pygame.init()
 pygame.display.set_caption("Jeu de BlackJack")
 FramePerSec = pygame.time.Clock()
 w = 800
-h = 600
+h = 800
 FPS = 60
 last_update = pygame.time.get_ticks()
 screen = pygame.display.set_mode((w, h))
 vec = pygame.math.Vector2
 tile_size = 50
 
+
+
 sprite_sheet_img = pygame.image.load('Game/assets/Sprites/doux.png')
 sprite_sheet = spriteSheet.SpriteSheet(sprite_sheet_img)
+
+bg_img = pygame.image.load('Game/assets/img/sky.png')
+sun_img = pygame.image.load('Game/assets/img/sun.png')
 
 def draw_grid():
     for line in range(0, 20):
         pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (w, line * tile_size))
         pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, h))
 
-def background(data):
-    screen.fill((130,201,230))
+class World():
+    def __init__(self, data):
+        self.tile_list = []
 
-    tile_list = []
-    grass_img = pygame.image.load('img/grass.png')
+        #load images
+        dirt_img = pygame.image.load('Game/assets/img/dirt.png')
+        grass_img = pygame.image.load('Game/assets/img/grass.png')
 
-    row_count = 0
-    for row in data:
-        col_count = 0
-        for tile in row:
-            if tile == 1:
-                img = pygame.transform.scale(grass_img, (tile_size, tile_size))
-                img_rect = img.get_rect()
-                img_rect.x = col_count * tile_size
-                img_rect.y = row_count * tile_size
-                tile = (img, img_rect)
-                tile_list.append(tile)
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
 
 world_data = [
     [0 ,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
@@ -59,9 +79,10 @@ world_data = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
+world = World(world_data)
 
 # Animation list
 animation_stand = []
@@ -77,9 +98,14 @@ for i in range(animation_steps):
 # Game loop
 def game():
     global frame, last_update
-    background(world_data)
-    draw_grid()
     while True:
+        screen.blit(bg_img, (0, 0))
+        screen.blit(sun_img, (100, 100))
+
+        world.draw()
+
+        draw_grid()
+        
         current_time = pygame.time.get_ticks()
         if current_time - last_update >= animation_cooldown:
             frame += 1
@@ -87,7 +113,7 @@ def game():
             if frame >= len(animation_stand):
                 frame = 0
 
-        screen.blit(animation_stand[frame], (100, h-100))
+        screen.blit(animation_stand[frame], (100, h-200))
 
         # Event handler
         for event in pygame.event.get():
